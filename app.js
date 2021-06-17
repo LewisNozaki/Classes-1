5e7b4b55636c065da1a63afa572eb7631ff73005
Lewis (Kenji) Nozaki <73324469+LewisNozaki@users.noreply.github.com>
2021-06-16 15:12:52 -1000

    Update app.js

--- app.js
+++ app.js
@@ -24,3 +24,48 @@ box.width = 20;
 console.log(box.getHeight()); // 10
 console.log(box.getWidth()); // 20
 
+// Class example 2
+class Human {
+  constructor(gender, name) {
+    this.gender = gender;
+    this.name = name;
+  }
+  
+  getGender() {
+    return this.gender;
+  }
+  
+  getName() {
+    return this.name;
+  }
+}
+
+class Person extends Human {
+  constructor(country, age) {
+    // super() will bring in all the constructor properties from the inherited class to be used.
+    super();
+    this.country = country;
+    this.age = age;
+  }
+  
+  getDetails() {
+    return `${this.name} is ${this.gender} that is ${this.age} years old and lives in ${this.country}.`
+  }
+}
+
+const franklin = new Person();
+
+franklin.gender = "boy";
+franklin.name = "Franklin Nozaki";
+franklin.age = 10;
+franklin.country = "USA";
+
+console.log(franklin.getDetails())
+
+
+
+
+
+
+
+
